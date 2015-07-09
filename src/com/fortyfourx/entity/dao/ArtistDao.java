b16683fe9486317262069ea35d27cb732a34a15a
Charith Arumapperuma <charithk44@gmail.com>
2015-07-09 20:23:16 +0530

    FORTYFOURX-UPDATE: DAO interfaces for Artist and Song are completed (upto current query requirements) and Javadoc is completed for them.

--- src/com/fortyfourx/entity/dao/ArtistDao.java
+++ src/com/fortyfourx/entity/dao/ArtistDao.java
@@ -4,11 +4,67 @@ import java.util.List;
 
 import com.fortyfourx.entity.Artist;
 
+/**
+ * @author Charith Arumapperuma
+ * <p>
+ * The DAO interface for {@link Artist} entity. Holds methods to read, write and 
+ * update {@code DATABASE.ARTIST} table.
+ */
 public interface ArtistDao {
+	
+	/**
+	 * Returns an {@link Artist} that is identified by {@code id}. This method must 
+	 * be implemented to execute an SQL query to select artist by id. Returns null 
+	 * if no such artist with the given id is found. 
+	 * <p>
+	 * @param id		Id of the artist that needs to be retrieved.
+	 * @return			One {@link Artist} instance or null.
+	 */
 	public Artist 		getArtistById(int id);
+	
+	/**
+	 * Returns a {@link List} of {@link Artist} elements that matches its name or a 
+	 * portion of the name to the given character sequence. This method must be 
+	 * implemented to execute an SQL query to search elements by name. 
+	 * <p>
+	 * @param name		Character sequence to be searched for.
+	 * @return			A {@link List} of {@link Artist} elements. 
+	 */
 	public List<Artist> getArtistsByName(String name);
+	
+	/**
+	 * Returns all the artists stored in the database.
+	 * <p>
+	 * @return			A {@link List} of {@link Artist} elements.
+	 */
 	public List<Artist> getAllArtists();
+	
+	/**
+	 * Returns URLs of all the artists stored in the database as a list of strings. 
+	 * <p>
+	 * @return			A {@link List} of {@link Artist} elements.
+	 */
 	public List<String> getAllArtistsUrls();
+	
+	/**
+	 * Returns the count of all artists stored in the database.
+	 * <p>
+	 * @return			An integer value of the count of artists.
+	 */
 	public int 			getAllArtistsCount();
+	
+	/**
+	 * Stores an artist to the database. Must implement INSERT query for the artist 
+	 * table. Method also checks whether artist is already present in the database 
+	 * and add any missing parameters of the {@link Artist} instance passed. Method 
+	 * will add a new record and return the {@link Artist} instance in its original 
+	 * state if it was not present in the database. If the object was present in the 
+	 * database and the passed object got updated with database data, the newly 
+	 * updated {@link Artist} object will be returned. In case of a failure to store 
+	 * artist, {@code null} will be returned.
+	 * <p>
+	 * @param artist	Artist needed to be added to the database.
+	 * @return			The same artist instance, or updated instance or {@code null}.
+	 */
 	public Artist 		addArtist(Artist artist);
 }
--- src/com/fortyfourx/entity/dao/SongDao.java
+++ src/com/fortyfourx/entity/dao/SongDao.java
@@ -5,10 +5,63 @@ import java.util.List;
 import com.fortyfourx.entity.Artist;
 import com.fortyfourx.entity.Song;
 
+/**
+ * @author Charith Arumapperuma
+ * <p>
+ * The DAO interface for {@link Song} entity. Holds methods to read, write and update 
+ * {@code DATABASE.SONG} table.
+ */
 public interface SongDao {
-	public Song getSongById(int id);
-	public List<Song> getSongsByTitle(String title);
-	public List<Song> getSongsByArtist(Artist artist);
-	public List<Song> getAllSongs();
-	public Song addSong(Song song);
+	
+	/**
+	 * Returns a {@link Song} that is identified by {@code id}. This method must be 
+	 * implemented to execute an SQL query to select song by id. Returns null if no 
+	 * such song with the given id is found. 
+	 * <p>
+	 * @param id		Id of the song that needs to be retrieved.
+	 * @return			One {@link Song} instance or null.
+	 */
+	public Song			getSongById(int id);
+	
+	/**
+	 * Returns a {@link List} of {@link Song} instances that matches its title or a 
+	 * portion of the title to the given character sequence. This method must be 
+	 * implemented to execute an SQL query to search elements by title. 
+	 * <p>
+	 * @param name		Character sequence to be searched for.
+	 * @return			A {@link List} of {@link Song} elements. 
+	 */
+	public List<Song>	getSongsByTitle(String title);
+	
+	/**
+	 * Returns a {@link List} of {@link Song} instances that the {@link Artist} passed 
+	 * as the parameter has sung. This method must be implemented to execute SQL query 
+	 * to search songs by artist id.
+	 * <p>
+	 * @param artist	{@link Artist} that needed to be searched.
+	 * @return			A {@link List} of {@link Song} elements.
+	 */
+	public List<Song>	getSongsByArtist(Artist artist);
+	
+	/**
+	 * Returns all the songs stored in the database as a {@link List} of {@link Song} instances.
+	 * <p>
+	 * @return			A {@link List} of {@link Song} instances.
+	 */
+	public List<Song>	getAllSongs();
+	
+	/**
+	 * Stores a song to the database. Must implement INSERT query for the song table. 
+	 * Method also checks whether song is already present in the database and if 
+	 * present, add any missing parameters of the {@link Song} instance passed.
+	 * Method returns the {@link Song} instance in its original state if it was not 
+	 * present in the database. If the instance was present in the database and the 
+	 * passed instance got updated with database data, the newly updated {@link Song} 
+	 * instance will be returned. In case of a failure to store song, {@code null} 
+	 * will be returned.
+	 * <p>
+	 * @param song		Song instance needed to be added to the database.
+	 * @return			The same song instance, or updated instance or {@code null}.
+	 */
+	public Song			addSong(Song song);
 }
